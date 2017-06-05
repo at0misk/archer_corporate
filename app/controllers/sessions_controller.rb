e946c7c08aeeefc73e5c3509c67f148670e7b423
at0misk <ktp925@gmail.com>
2017-06-05 12:15:17 -0700

    views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
 	def root
 	end
+	def traveler_profile
+	end
+	def about
+	end
+	def frequent
+	end
+	def contact
+	end
+	def contact_process
+		flash[:sent] = "Thank you!  Your message has been sent."
+		redirect_to '/contact'
+	end
+	def terms
+	end
+	def privacy
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 	root 'sessions#root'
+	get 'traveler_profile' => 'sessions#traveler_profile'
+	get 'about' => 'sessions#about'
+	get 'frequent_traveler_program' => 'sessions#frequent'
+	get 'contact' => 'sessions#contact'
+	post 'contact' => 'sessions#contact_process'
+	get 'terms' => 'sessions#terms'
+	get 'privacy' => 'sessions#privacy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
